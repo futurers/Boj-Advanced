189fc128c84f391d35b80bc179f820ad566f4683
shinyangdevelop <shinyangdevelop@gmail.com>
2022-11-09 00:25:04 +0900

    Add remove-ads-button to get and set data, and added data change listener in background

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,5 +1,14 @@
-let removeAdsButton = document.getElementById("remove-ads-button")
-
-removeAdsButton.addEventListener("click", () => {
-    console.log("Remove ads button clicked")
-})
+window.onload = () => {
+    let removeAdsButton = document.getElementById("remove-ads-button")
+    setInterval(() => {
+        chrome.storage.sync.get('ads-block').then((value) => {
+            removeAdsButton.innerHTML = value['ads-block']?"Remove ads":"Show ads"
+        })
+    }, 10)
+    removeAdsButton.addEventListener("click", () => {
+        console.log("Remove ads button clicked");
+        chrome.storage.sync.get('ads-block').then((value) => {
+            chrome.storage.sync.set({'ads-block': !value['ads-block']})
+        })
+    })
+}
